f49c6bff2d83bf813e90fbff66c9da245b34e4f7
Michael Gudowski <michaelgudowski@gmail.com>
2015-01-26 18:56:15 -0600

    Updating todo list.

--- public/todo_list.php
+++ public/todo_list.php
@@ -2,28 +2,41 @@
 define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'todo_db');
 define('DB_USER', 'codeup');
-define('DB_PASS', '');
-// insert password
+define('DB_PASS', 'password');
 
 
 require('../db_connect.php');
 
-function gettodo($dbc) {
-    // Bring the $dbc variable into scope 
-    return $dbc->query('SELECT * FROM todolist')->fetchAll(PDO::FETCH_ASSOC);
+//pagination if isset statement
+if (!isset($_GET['page']){
+
+} else {
+
+};
+
+$offset = 0;
+
+if (isset($_GET['remove'])) {
+		$id = $_GET['remove'];
+	$stmt = $dbc->prepare("DELETE FROM todolist WHERE id = :id");
+	$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+	$stmt->execute();
+
+}
 }// add limit
 // adding the todo data field into the database
 if (!empty($_POST["todo"])) {
-	$query = "INSERT INTO todolist (things_to_do) VALUES (:todo)";
+	$query = "INSERT INTO todolist (things_to_do) VALUES (:things_to_do)";
     $stmt = $dbc->prepare($query);
-	$stmt->bindValue(':todo', $_POST['todo'], PDO::PARAM_STR);
+	$stmt->bindValue(':things_to_do', $_POST['things_to_do'], PDO::PARAM_STR);
 	$stmt->execute();	
 }
+//still needs work
+$todos = $dbc->prepare("SELECT * FROM todolist' LIMIT 10");
+$todos->bindValue('')''
+$todos->execute();
+$posts = $todos->fetchAll(PDO::FETCH_ASSOC);
 
-
-$offset = 0;
-$todo = gettodo($dbc);
- 
 ?>
 
 
@@ -49,11 +62,15 @@ $todo = gettodo($dbc);
 
 		<th>To Do Item</th>
 
+		<!-- foreach loop with the values in the database to display on the page -->
 
-		<? foreach($todo as $key => $value): ?>
+		<? foreach($posts as $value): ?>
 				<tr>
 					<td>
 						<?= $value['things_to_do'] ?>
+						
+							<a href=?remove=<?=$key?>><span class="glyphicon glyphicon-remove-sign" aria-hidden="true"></span></a>
+						
 					</td>	
 			 	
 				</tr>
@@ -63,7 +80,7 @@ $todo = gettodo($dbc);
 
 			<form method="POST" action="todo_list.php">
 
-	       	<p>
+	       		<p>
 	            	<label for="todo"></label>
 					<input type="text" id="todo" name="todo" value="" placeholder="List Item">
 	        	</p>
